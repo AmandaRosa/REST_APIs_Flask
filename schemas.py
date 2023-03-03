554b94d23613ae57818fc35c2af570be659c5c84
AmandaRosa <amandarosafj@live.com>
2023-03-03 10:17:43 -0300

    data validation with marshmallow

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields
+
+class ItemSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    store_id = fields.Str(required=True)
+
+
+class ItemSchemaUpdate(Schema):
+    name = fields.Str()
+    price = fields.Float()
+
+
+class StoresSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
